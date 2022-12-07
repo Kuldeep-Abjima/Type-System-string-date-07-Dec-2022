ff96f724be7fdc671d8537969c133fc7b1cf5d6a
Kuldeep-Abjima <119651692+Kuldeep-Abjima@users.noreply.github.com>
2022-12-07 18:26:05 +0530

    Add files via upload

---
+++ DateTimeWithCharDefine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace dec072022{
+    public class DateTimeWithCharDefine
+    {
+      static void Main(String[] args)
+        {
+            Console.WriteLine("enter the charcter");
+            char c = Convert.ToChar(Console.ReadLine());
+            
+            bool isWhiteSpace = char.IsWhiteSpace(c);
+            bool isDigit = char.IsDigit(c);
+            bool isPunctuation = char.IsPunctuation(c);
+            bool isNum = char.IsNumber(c);
+            bool isLetter = char.IsLetter(c);
+            bool issym = char.IsSymbol(c);
+            Console.WriteLine($"bool is digit : {isDigit},\n punctuation : {isPunctuation},\n whitespace : {isWhiteSpace},\n num :{isNum},\n letter : {isLetter}, \n Symbol : {issym}");
+            DateTime hireDate = new DateTime(2022, 12, 07, 14, 28, 0);
+            DateTime joinDate = hireDate.AddDays(16);
+            DateTime todayDate = DateTime.Now;
+            Console.WriteLine($"Hire Date and Time is : {hireDate} \n joining date: {joinDate} \n todayDate : {todayDate}");
+            DateTime startWork = DateTime.Now;
+            TimeSpan workTime = new TimeSpan(09, 00, 00);
+            DateTime endWork = startWork.Add(workTime);
+            Console.WriteLine(startWork.ToLongDateString());
+            Console.WriteLine(endWork.ToLongTimeString());
+
+
+        }
+        
+    }
+
+    }
---
+++ stringClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Dec072022
+{
+    public class stringClass
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter the text");
+            var text = Console.ReadLine();
+            Console.WriteLine(text);
+            int k = text.Length;
+            string[] a = new string[k];
+
+            for (int i = 0; i < k; i++)
+            {
+                {
+
+                    a[i] = text.Substring(0, i + 1);
+
+
+
+                    //for(int i = 0; i < a.Length; i++)
+                    //{
+                    //    Console.Write($"Partation of array : {a[i]},");
+                    //}
+   
+
+
+                }
+            }
+            Console.WriteLine($"[{string.Join(",", a)}]");
+        }
+    }
+}
